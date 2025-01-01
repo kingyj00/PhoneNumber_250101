165a68e62ae288993a5fee9971533693286628c3
kingyj00 <onshopyj@gmail.com>
2025-01-01 23:18:20 +0900

    Step8

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -44,14 +44,14 @@ class App {
                 System.out.print(" 전화번호 : ");
                 String author = scanner.nextLine();
                 int id = wiseSayings.size() + 1;
-                wiseSayings.add(new WiseSaying(id, author, content));
+                wiseSayings.add(new WiseSaying(id, content, author));
                 System.out.println(" %d번에 명언이 등록되었습니다.".formatted(id));
             } else if (cmd.equals("목록")) {
                 System.out.println("번호 / 이름 / 전화번호");
                 System.out.println("===================");
                 for (int i = wiseSayings.size() - 1; i >= 0; i--) {
                     WiseSaying wiseSaying = wiseSayings.get(i);
-                    System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.author, wiseSaying.content));
+                    System.out.println("%d / %s / %s".formatted(wiseSaying.id, wiseSaying.content, wiseSaying.author));
                 }
             } else if (cmd.equals("삭제")) {
                 System.out.print("삭제할 번호 : ");
@@ -72,6 +72,32 @@ class App {
                     for (int i = 0; i < wiseSayings.size(); i++) {
                         wiseSayings.get(i).id = i + 1;
                     }
+                }
+            } else if (cmd.equals("수정")) {
+                // 명언 수정
+                System.out.print("수정할 번호 : ");
+                int updateId = Integer.parseInt(scanner.nextLine());
+                boolean found = false;
+
+                // 수정할 명언 찾기
+                for (int i = 0; i < wiseSayings.size(); i++) {
+                    if (wiseSayings.get(i).id == updateId) {
+                        // 수정할 명언의 내용과 작가를 입력받음
+                        System.out.print("새로운 이름 : ");
+                        String newContent = scanner.nextLine();
+                        System.out.print("새로운 전화번호 : ");
+                        String newAuthor = scanner.nextLine();
+
+                        // 수정된 내용으로 업데이트
+                        wiseSayings.get(i).author = newAuthor;
+                        wiseSayings.get(i).content = newContent;
+                        System.out.println("%d번 전화번호가 수정되었습니다.".formatted(updateId));
+                        found = true;
+                        break;
+                    }
+                }
+                if (!found) {
+                    System.out.println("해당 번호에 저장된 전화번호가 없습니다.");
                 }
             }
         }

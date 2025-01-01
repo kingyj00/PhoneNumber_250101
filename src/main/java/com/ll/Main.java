a3553580385dfcad9dc841bfb0a4c488f5463abf
kingyj00 <onshopyj@gmail.com>
2025-01-01 22:47:28 +0900

    Step5

--- src/main/java/com/ll/Main.java
+++ src/main/java/com/ll/Main.java
@@ -1,38 +1,45 @@
 package com.ll;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
-    public static void main (String[] args){
+    public static void main( String[] args){
         App app = new App();
         app.run();
     }
 }
 
 class App {
-    public void run(){
+    public void run() {
         System.out.println(" ================");
         System.out.println(" == 전화번호부 ==");
         System.out.println(" ================");
 
         Scanner scanner = new Scanner(System.in);
-
         int lastId = 0;
+        ArrayList<String> wiseSayings = new ArrayList<>();
 
-        while (true){
+        while (true) {
             System.out.print(" 입력 : ");
-            String a = scanner.nextLine();
+            String cmd = scanner.nextLine();
 
-            if (a.equals("종료")) {
+            if (cmd.equals("종료")) {
                 break;
-            } else if (a.equals("등록")){
-                System.out.print("이름 :");
+            } else if (cmd.equals("등록")) {
+                System.out.print(" 이름 : ");
                 String content = scanner.nextLine();
-                System.out.print("번호 :");
+                System.out.print(" 전화번호 : ");
                 String author = scanner.nextLine();
                 int id = ++lastId;
-                System.out.println("%d번 번호가 등록되었습니다.".formatted(id));
-                id++;
+                wiseSayings.add(id + "/" + content + "/" + author);
+                System.out.println(" %d번에 번호가 등록되었습니다.".formatted(id));
+            } else if (cmd.equals("목록")){
+                System.out.println("번호 / 이름 / 전화번호");
+                System.out.println("===================");
+                for (int i = wiseSayings.size() - 1; i >=0; i--) {
+                    System.out.println(wiseSayings.get(i));
+                }
             }
         }
         scanner.close();
